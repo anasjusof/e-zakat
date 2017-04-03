a72cc14908f52e6e0a50f17804cebbedc28730d3
anas.jusof <anas.jusof@selectv.co>
2017-04-03 12:04:43 +0800

    Fix redirect

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -29,6 +29,7 @@ class AuthController extends Controller
      * @var string
      */
     protected $redirectTo = '/admin-dashboard';
+    public $redirectAfterLogout='login';
 
     /**
      * Create a new authentication controller instance.
